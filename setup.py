9e8432c881d11c4786a17009f61993844eaa49df
Jared Hobbs <jared@pyhacker.com>
2025-12-03 11:37:47 -0700

    update version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ PACKAGES = find_packages()
 
 setup(
     name=PROJECT_PACKAGE_NAME,
-    version='0.1.6',
+    version='0.2.0',
     url=PROJECT_URL,
     project_urls=PROJECT_URLS,
     author=PROJECT_AUTHOR,
